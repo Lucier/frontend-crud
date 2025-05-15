20b5362f182d13bfddf18980bbb071620fa87207
Lucier <lucierflima@gmail.com>
2025-05-15 08:57:05 -0300

    Add function delete

--- src/components/User/Edit.tsx
+++ src/components/User/Edit.tsx
@@ -2,8 +2,6 @@ import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import axios from 'axios'
 
-// type User = z.infer<typeof User> & { id: string }
-
 export function Edit() {
   const navigate = useNavigate()
   const { id } = useParams()
--- src/components/User/Users.tsx
+++ src/components/User/Users.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import type IUsers from '../../interfaces/IUsers'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
+import Swal from 'sweetalert2'
 
 function Home() {
   const [users, setUsers] = useState<IUsers[]>([])
@@ -13,6 +14,30 @@ function Home() {
   async function loadUsers() {
     const response = await axios.get('http://localhost:3333/api/v1/users')
     setUsers(response.data)
+  }
+
+  async function deleteUser(id: string) {
+    const confirmDelete = await Swal.fire({
+      title: 'Tem certeza?',
+      text: 'Esta ação não pode ser desfeita!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#d33',
+      cancelButtonColor: '#3085d6',
+      confirmButtonText: 'Sim, Excluir',
+      cancelButtonText: 'Cancelar',
+    })
+
+    if (!confirmDelete.isConfirmed) return
+
+    await axios
+      .delete(`http://localhost:3333/api/v1/users/${id}`)
+      .then(() => {
+        setUsers(users.filter((u) => id !== u.id))
+      })
+      .catch((error) => {
+        console.log('Erro ao excluir usuário: ' + error)
+      })
   }
 
   return (
@@ -34,6 +59,7 @@ function Home() {
                   <td>{u.email}</td>
                   <td>
                     <Link to={`/edit-user/${u.id}`}>EDITAR</Link>
+                    <button onClick={() => deleteUser(u.id)}>EXCLUIR</button>
                   </td>
                 </tr>
               )
